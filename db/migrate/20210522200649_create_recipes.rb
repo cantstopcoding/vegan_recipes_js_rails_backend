73977f1690713514383fc0190615a8cd82b7495d
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-22 16:08:18 -0400

    create category scaffold

---
+++ db/migrate/20210522200649_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :image_url
+      t.string :ingredients
+      t.text :instructions
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# create_table "recipes", force: :cascade do |t|
+#     t.string "title"
+#     t.string "image_url"
+#     t.string "ingredients"
+#     t.text "instructions"
+#     t.integer "user_id", null: false
+#     t.integer "category_id", null: false
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#     t.index ["category_id"], name: "index_recipes_on_category_id"
+#     t.index ["user_id"], name: "index_recipes_on_user_id"
