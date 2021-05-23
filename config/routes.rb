4cf6d0f0c8a7affe52e4ce7ec3cc42fadde435cb
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-23 08:31:28 -0400

    nest recipes route in api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :categories
+  # resources :categories
+
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
